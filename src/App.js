8c125875d58c5cf7114b17604127145948e44762
Michael <mikek222@gmail.com>
2025-03-13 17:11:14 -0400

    updated with movie cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Header from './components/Header';
 import './App.css';
 import './styles.css'
 import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header />
+      </div>
+      <div>
+        <MoviesGrid />
       </div>
         <Footer />
     </div>
